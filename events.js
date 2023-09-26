73afcfb90013ada82099b5272279e09537d97bda
Matt Young <127060152+myoung24@users.noreply.github.com>
2023-09-26 21:02:10 -0230

    node package installed

--- events.js
+++ events.js
@@ -1,2 +1,8 @@
-var events = require('events');
+var events = require("events");
 var eventEmitter = new events.EventEmitter();
+
+eventEmitter.on("emit", () => {
+  console.log("Event triggered");
+});
+
+eventEmitter.emit("emit");
